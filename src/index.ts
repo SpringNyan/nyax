8715cba335f8e2e998b2ae1d4d1495bff34247e2
SpringNyan <springnyan@gmail.com>
2020-04-19 00:04:33 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { createRequiredArg } from "./arg";
 export { createSubContainer } from "./container";
 export { createDefaultModel, flattenModels } from "./model";
+export { createSelector } from "./selector";
 export { createNyax } from "./store";
