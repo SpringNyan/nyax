1976f213a702ff1ac03f01b09583997b7c99ab23
SpringNyan <springnyan@gmail.com>
2020-04-19 00:10:47 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+export type { Action, ActionHelper, AnyAction } from "./action";
 export { createRequiredArg } from "./arg";
 export { createSubContainer } from "./container";
+export type { Container, GetContainer } from "./container";
 export { createDefaultModel, flattenModels } from "./model";
+export type { ModelConstructor, ModelConstructors } from "./model";
 export { createSelector } from "./selector";
 export { createNyax } from "./store";
+export type { Nyax, NyaxOptions } from "./store";
